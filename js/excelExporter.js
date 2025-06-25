921f13ced331ec0be47f75a32224de54cd9b4df5
Ivan Silva Alves <139565955+Ivan-ssa@users.noreply.github.com>
2025-06-25 15:06:18 -0300

    Update excelExporter.js

--- js/excelExporter.js
+++ js/excelExporter.js
@@ -7,7 +7,6 @@ export const exportTableToExcel = (data, filename = 'export_data') => {
     }
 
     // Define os cabeçalhos que queremos exportar e a ordem
-    // Eles devem corresponder exatamente às chaves dos objetos em 'data'
     const headers = [
         "TAG",
         "Equipamento",
@@ -29,14 +28,9 @@ export const exportTableToExcel = (data, filename = 'export_data') => {
         return row;
     });
 
-    // Adiciona os cabeçalhos como a primeira linha do array de dados
     const ws = XLSX.utils.json_to_sheet(exportRows, { header: headers });
-
-    // Cria um novo workbook (pasta de trabalho)
     const wb = XLSX.utils.book_new();
-    // Adiciona a planilha ao workbook com o nome "Resultados"
     XLSX.utils.book_append_sheet(wb, ws, "Resultados");
 
-    // Gera o arquivo Excel e inicia o download
     XLSX.writeFile(wb, `${filename}.xlsx`);
 };
