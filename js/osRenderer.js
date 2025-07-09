59c1b7fe7aaeaf1a820708b01b125701d7b74215
Ivan Silva Alves <139565955+Ivan-ssa@users.noreply.github.com>
2025-07-09 09:17:09 -0300

    mudança nos menus

--- js/osRenderer.js
+++ js/osRenderer.js
@@ -91,6 +91,7 @@ export function renderOsTable(
         row.insertCell().textContent = os.Equipamento ?? '';
         row.insertCell().textContent = os.Modelo ?? '';
         row.insertCell().textContent = os.Fabricante ?? '';
+        row.insertCell().textContent = equipment.Setor ?? '';
         // row.insertCell().textContent = osStatusCalib; 
         // row.insertCell().textContent = osStatusManutencao; 
     });
